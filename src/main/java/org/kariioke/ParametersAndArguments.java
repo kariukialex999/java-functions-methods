090f82347d547fe0e0e76d9462b3d628cb90b45d
Alex Kariuki <kariukialex999@gmail.com>
2026-02-07 12:43:02 +0300

    parameters and arguments

---
+++ src/main/java/org/kariioke/ParametersAndArguments.java
@@ -0,0 +1,27 @@
+package org.kariioke;
+
+import java.util.Scanner;
+
+public class ParametersAndArguments {
+    static void main() {
+        //we can pass the values of the numbers we want to work on as we call the function
+        int result = sum(2,7);
+        System.out.println(result);
+    }
+
+//    static int sum1() {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter the first number: ");
+//        int firstNum = scanner.nextInt();
+//        System.out.println("Enter the second number: ");
+//        int secondNum = scanner.nextInt();
+//        int sum = firstNum + secondNum;
+////        System.out.println("The sum of the two numbers is: " + sum);
+//        return sum;
+//    }
+
+    static int sum(int i, int j) {
+        int sum = i + j;
+        return sum;
+    }
+}
